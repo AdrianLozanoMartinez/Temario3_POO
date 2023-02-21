b04b8c11341035ef1158feca9958717957094c87
adrian <adrianlm1985@gmail.com>
2023-02-21 17:02:51 +0100

    Copia de seguridad

--- LinQ/Program.cs
+++ LinQ/Program.cs
@@ -72,6 +72,30 @@ class Program
         var paresPares2 = numeros.Select(EsPar).ToArray();
 
 
+        //20-02-2024
+        //customer.where(cust => cust.City == "London").OrderBy Descending(cust => cust.Name).Select(cust => cust.ToString.Tolower())
+        //Console.ReadKey(); -> Espera una tecla para salir o que haga una acción
+        //order by es ascendente por defecto
+        //join une un dato en comun entre dos tablas que no estaban relacionadas
+        //group agrupa segun una condicion, si es ciudad ya te lo agrupa si es de malaga, marbella, algecias...
+        //pg12 -> custoGro..Key 1º muestra la ciudad(marbella...) y el customer.name -> nombre de cliente, where gru.count > 2 grupo que tenga mas de 2 clientes
+        //customer.GroupBy(c=>c.City.where(g=>g.Count()>2).OrderBy(g=>g.Key)
+        //Count()-> Método que mira si es array usa lenght si es lista cont sino lo tiene que contar manualmente
+        //No hay nulo en LYQuest, las colecciones no pueden ser nulos pero si puede ser vacía o llevar elementos(esos elementos pueden ser nulo)
+        //En where se puede poner que devuelva elementos que no sean nulos, y ahorramos el if -> where cust != null && cust.city...Se comprueba siempre primero si es null
+        //pg 16-> customer.where(c=>c.NameStartWith("AB")).OrderBy(c=>c.Name).Select(C=>c.NAmeToUpper()); -> es igual que si en base dice que empiece en AB-> Name LIKE 'AB%'
+        //Si ponemos dos where...despues del select-> where((numero,indice) => indice >= 21 && indice <= 30) o otra opción mejor
+        //.Select(C=>c.NAmeToUpper()).Skip(21).Take(9); ->Skip son los 21 primeros incluyendo el 21 y coge los 9. Este es más efectivo que el de arriba, porque el de arriba coje todos los elementos(si hay un millon lo coge) pero este solo los 21
+        //Podemos meter en un metodo:
+        /*
+         IEnumerable<string> Customer(string start) //manda la letra que va a empezar
+         customer.where(c=>c.NameStartWith(start))
+                 .OrderBy(c=>c.Name)
+                 .Select(C=>c.NAmeToUpper()).Skip(21).Take(9);
+         
+         */
+
+        //cuando tiene clave foranea no se hace join...ECHADO FOTO -> Customer son la tabla de cliente. pg19 -> IGNORAR AHORA HASTA EL ÚLTIMO TEMA
 
 
 
